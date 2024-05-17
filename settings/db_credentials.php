30e7124bc3d06741187fc8f7b908e51012860da3
Michal Šiman <michal.siman@gmail.com>
2024-05-17 10:51:10 +0200

    Create db_credentials.php

---
+++ settings/db_credentials.php
@@ -0,0 +1,11 @@
+<?php
+//
+// credentials for MySQLi connection to MySQL db
+//
+$host    = 'localhost';
+$user    = ''; // db user
+$pass    = ''; // db password
+$db      = ''; // db name
+$port    = '3306'; // db port
+$charset = 'utf8mb4'; // db charset
+$collation = 'utf8mb4_general_ci'; // db collation
